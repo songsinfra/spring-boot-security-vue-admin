030030468993d4ff492c64e97a252d334ab9e68d
kim <sukim80@nate.com>
2019-11-22 19:01:19 +0900

    - delete : 필요없는 파일과 코드 삭제

--- src/main/front/nuxt.config.js
+++ src/main/front/nuxt.config.js
@@ -3,18 +3,12 @@ console.log("webpack config :" , process.env.npm_lifecycle_event);
 module.exports = {
     head: {
         script: [
-            // { src:'/vendors/js/vendors.min.js'},
-            // { src:'/js/core/app-menu.js'},
              {src: '/js/core/libraries/jquery.min.js'}
 
         ],
         link: [
             {rel: 'apple-touch-icon', href: '/images/ico/apple-icon-120.png'},
             {rel: 'shortcut icon', href: '/images/ico/favicon.ico'},
-            {
-                rel: 'stylesheet',
-                href: 'https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i%7CComfortaa:300,400,700'
-            },
             {
                 rel: 'stylesheet', href: '/fonts/simple-line-icons/style.css'
             },
@@ -28,8 +22,6 @@ module.exports = {
             {rel: 'stylesheet', href: '/css/app.css'},
             {rel: 'stylesheet', href: '/css/core/menu/menu-types/vertical-menu.css'},
             {rel: 'stylesheet', href: '/css/core/colors/palette-gradient.css'},
-            {rel: 'stylesheet', href: '/css/pages/chat-application.css'},
-            {rel: 'stylesheet', href: '/css/pages/dashboard-analytics.css'},
         ],
     },
     build: {
